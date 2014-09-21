668e13d8a01b7393dfd51b9a2f3a549222dce295
Maxim Oleinyk <maxim.oleinyk@gmail.com>
2014-09-21 19:50:03 +0300

    Fixed edit state issue

--- src/client/js/util/grid/table/content.js
+++ src/client/js/util/grid/table/content.js
@@ -1,72 +1,93 @@
 define(function (require) {
 
-    var Marionette = require('marionette'),
-        EditRow = require('./rows/editRow'),
-        ViewRow = require('./rows/viewRow');
+	var Marionette = require('marionette'),
+		EditRow = require('./rows/editRow'),
+		ViewRow = require('./rows/viewRow');
 
-    return Marionette.CollectionView.extend({
+	return Marionette.CollectionView.extend({
 
-        template: require('hbs!./content'),
-        itemView: true,
+		template: require('hbs!./content'),
+		itemView: true,
 
-        // @Override
-        renderItemView: function(view, index) {
-            view.render(index);
-            this.appendHtml(this, view, index);
-        },
+		initialize: function() {
+			this.order = {};
+		},
 
-        buildItemView: function(model, View, options) {
-            var ItemView,
-				state = this.getState(options);
+		// @Override
+		renderItemView: function (view, index) {
+			view.render(index);
+			this.appendHtml(this, view, index);
+		},
 
-            if (state === 'view') {
-                ItemView = ViewRow;
-            } else if (state === 'edit') {
-                ItemView = EditRow;
-            } else {
-                throw 'Unknown state: ' + this.options.state;
-            }
+		// @Override
+		appendHtml: function (collectionView, itemView, index) {
+			var children = collectionView.$el.children();
 
-            var row = new ItemView({
-                model: model,
-                columns: this.options.columns,
-                items: this.options.collection,
-                editable: this.options.editable,
-				onSave: this.options.onSave,
-                state: state
-            });
+			if (collectionView.isBuffering) {
+				collectionView.elBuffer.appendChild(itemView.el);
+				collectionView._bufferedChildren.push(itemView);
+			} else {
+				if (index > 0) {
+					children.eq(index - 1).after(itemView.el);
+				} else {
+					if (children.length) {
+						children.eq(index).before(itemView.el);
+					} else {
+						collectionView.$el.append(itemView.el);
+					}
+				}
 
-			row.on('change:state', _.bind(this.handleStateChange, this));
-
-			return row;
-        },
-
-		itemViewOptions: function() {
-			return {
-				state: this.getState()
 			}
 		},
 
-		getState: function(options) {
+		buildItemView: function (model, View, options) {
+			var ItemView,
+				state = this.getState(options);
+
+			if (state === 'view') {
+				ItemView = ViewRow;
+			} else if (state === 'edit') {
+				ItemView = EditRow;
+			} else {
+				throw 'Unknown state: ' + this.options.state;
+			}
+
+			var row = new ItemView({
+				model: model,
+				columns: this.options.columns,
+				items: this.options.collection,
+				editable: this.options.editable,
+				onSave: this.options.onSave,
+				state: state
+			});
+
+			this.order[model.cid] = options.index;
+			row.on('change:state', _.bind(this.handleStateChange, this));
+
+			return row;
+		},
+
+		itemViewOptions: function (model, index) {
+			return {
+				state: this.getState(),
+				index: index
+			}
+		},
+
+		getState: function (options) {
 			options = options || {};
 
 			return options.state ? options.state : this.options.state;
 		},
 
-		handleStateChange: function(state, view, model) {
-			var index = -1,
+		handleStateChange: function (state, view, model) {
+			var index = this.order[model.cid],
 				originItemViewOptions = this.itemViewOptions;
 
-			this.children.find(function(v, i) {
-				if (v.cid === view.cid) {
-					index = i;
-					return v;
-				}
-			});
-
-			this.itemViewOptions = function() {
+			this.itemViewOptions = function () {
 				return {
-					state: state
+					state: state,
+					index: index
 				}
 			};
 			this.removeItemView(model);
@@ -74,5 +95,5 @@ define(function (require) {
 			this.itemViewOptions = originItemViewOptions;
 		}
 
-    });
+	});
 });
